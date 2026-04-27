6f9ac26fdc52840455a6f54dbeda599faa6344a7
hillaryokoth <77391453+hillaryokoth@users.noreply.github.com>
2026-04-27 12:28:59 +0300

    Enhance server with URL-encoded support and CORS
    Added middleware for URL-encoded data and CORS headers.

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
 // ─── CONFIGURATION ───────────────────────────────────────────────────────────
 const CONSUMER_KEY = "vJkP76ozIxQzD3QnPJUGBCnVjTtkr5QK6g5MwAGMGGEKAlmp";
